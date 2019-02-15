8f0536a8d6ead5f85343d845fff7e2a6d87b778c
ACH <aleksandra.dolega@capgemini.com>
2019-02-15 10:56:42 +0100

    [ACH] Get gifs urls

--- js/services/importExportService.js
+++ js/services/importExportService.js
@@ -1,205 +1,227 @@
 'use strict';
 
 angular
-  .module('fireideaz')
-  .service('ImportExportService', 
-          ['FirebaseService', 'ModalService', 'CsvService', '$filter', 
-          function (firebaseService, modalService, CsvService, $filter) {
-    var importExportService = {};
+    .module('fireideaz')
+    .service('ImportExportService',
+        ['FirebaseService', 'ModalService', 'CsvService', '$filter', '$http',
+            function (firebaseService, modalService, CsvService, $filter, $http) {
+                var importExportService = {};
 
-    importExportService.importMessages = function (userUid, importObject, messages) {
-      var data = importObject.data;
-      var mapping = importObject.mapping;
+                importExportService.getMemesUrlsByQuery = function (name, offset) {
+                    if (!offset) {
+                        offset = 0;
+                    }
+                    return $http({
+                        method: 'GET',
+                        url: 'https://api.giphy.com/v1/gifs/search?api_key=orXMkeCrlZ1aZZLEVLWCjY7XsUgYgJUe&limit=10&q=' + name + '&offset=' + offset
+                    }).then(function successCallback(response) {
+                        return response.data.data.map(function (gifObj) {
+                            return gifObj.images.downsized.url;
+                        });
+                    }, function errorCallback(response) {
+                        console.log('ERROR: '+response);
+                        return [];
+                    });
+                };
 
-      for (var importIndex = 1; importIndex < data.length; importIndex++) {
-        for (var mappingIndex = 0; mappingIndex < mapping.length; mappingIndex++) {
-          var mapFrom = mapping[mappingIndex].mapFrom;
-          var mapTo = mapping[mappingIndex].mapTo;
+                importExportService.importMessages = function (userUid, importObject, messages) {
+                    var data = importObject.data;
+                    var mapping = importObject.mapping;
 
-          if (mapFrom === -1) {
-           continue;
-         }
+                    for (var importIndex = 1; importIndex < data.length; importIndex++) {
+                        for (var mappingIndex = 0; mappingIndex < mapping.length; mappingIndex++) {
+                            var mapFrom = mapping[mappingIndex].mapFrom;
+                            var mapTo = mapping[mappingIndex].mapTo;
 
-          var cardText = data[importIndex][mapFrom];
+                            if (mapFrom === -1) {
+                                continue;
+                            }
 
-          if (cardText) {
-             messages.$add({
-             text: cardText,
-             user_id: userUid,
-             type: {
-               id: mapTo
-             },
-             date: firebaseService.getServerTimestamp(),
-             votes: 0});
-          }
-        }
-      }
+                            var cardText = data[importIndex][mapFrom];
 
-      modalService.closeAll();
-    };
+                            if (cardText) {
+                                messages.$add({
+                                    text: cardText,
+                                    user_id: userUid,
+                                    type: {
+                                        id: mapTo
+                                    },
+                                    date: firebaseService.getServerTimestamp(),
+                                    votes: 0
+                                });
+                            }
+                        }
+                    }
 
-    importExportService.getSortFields = function(sortField) {
-      return sortField === 'votes' ? ['-votes', 'date_created'] : 'date_created';
-    };
+                    modalService.closeAll();
+                };
 
-    importExportService.getBoardText = function(board, messages, sortField) {
-      if (board) {
-        var clipboard = '';
+                importExportService.getSortFields = function (sortField) {
+                    return sortField === 'votes' ? ['-votes', 'date_created'] : 'date_created';
+                };
 
-        $(board.columns).each(function(index, column) {
-          if (index === 0) {
-            clipboard += '<strong>' + column.value + '</strong><br />';
-          } else {
-            clipboard += '<br /><strong>' + column.value + '</strong><br />';
-          }
+                importExportService.getBoardText = function (board, messages, sortField) {
+                    if (board) {
+                        var clipboard = '';
 
-          var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
+                        $(board.columns).each(function (index, column) {
+                            if (index === 0) {
+                                clipboard += '<strong>' + column.value + '</strong><br />';
+                            } else {
+                                clipboard += '<br /><strong>' + column.value + '</strong><br />';
+                            }
 
-          $(filteredArray).each(function(index2, message) {
-            if (message.type.id === column.id) {
-              clipboard += '- ' + message.text + ' (' + message.votes + ' votes) <br />';
-            }
-          });
-        });
+                            var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
 
-        return clipboard;
-      }
+                            $(filteredArray).each(function (index2, message) {
+                                if (message.type.id === column.id) {
+                                    clipboard += '- ' + message.text + ' (' + message.votes + ' votes) <br />';
+                                }
+                            });
+                        });
 
-      return '';
-    };
+                        return clipboard;
+                    }
 
-    importExportService.getBoardPureText = function(board, messages, sortField) {
-      if (board) {
-        var clipboard = '';
+                    return '';
+                };
 
-        $(board.columns).each(function(index, column) {
-          if (index === 0) {
-            clipboard += column.value + '\n';
-          } else {
-            clipboard += '\n' + column.value + '\n';
-          }
+                importExportService.getBoardPureText = function (board, messages, sortField) {
+                    if (board) {
+                        var clipboard = '';
 
-          var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
+                        $(board.columns).each(function (index, column) {
+                            if (index === 0) {
+                                clipboard += column.value + '\n';
+                            } else {
+                                clipboard += '\n' + column.value + '\n';
+                            }
 
-          $(filteredArray).each(function(index2, message) {
-            if (message.type.id === column.id) {
-              clipboard += '- ' + message.text + ' (' + message.votes + ' votes) \n';
-            }
-          });
-        });
+                            var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
 
-        return clipboard;
-      }
+                            $(filteredArray).each(function (index2, message) {
+                                if (message.type.id === column.id) {
+                                    clipboard += '- ' + message.text + ' (' + message.votes + ' votes) \n';
+                                }
+                            });
+                        });
 
-      return '';
-    };
+                        return clipboard;
+                    }
 
-    importExportService.submitImportFile = function (file, importObject, board, scope) {
-      importObject.mapping = [];
-      importObject.data = [];
+                    return '';
+                };
 
-      if (file) {
-        if (file.size === 0) {
-          importObject.error = 'The file you are trying to import seems to be empty';
-          return;
-        }
+                importExportService.submitImportFile = function (file, importObject, board, scope) {
+                    importObject.mapping = [];
+                    importObject.data = [];
 
-        /* globals Papa */
-        Papa.parse(file, {
-          complete: function(results) {
-            if (results.data.length > 0) {
-              importObject.data = results.data;
+                    if (file) {
+                        if (file.size === 0) {
+                            importObject.error = 'The file you are trying to import seems to be empty';
+                            return;
+                        }
 
-              board.columns.forEach (function (column){
-                importObject.mapping.push({ mapFrom: '-1', mapTo: column.id, name: column.value });
-              });
+                        /* globals Papa */
+                        Papa.parse(file, {
+                            complete: function (results) {
+                                if (results.data.length > 0) {
+                                    importObject.data = results.data;
 
-              if (results.errors.length > 0) {
-                 importObject.error = results.errors[0].message;
-              }
+                                    board.columns.forEach(function (column) {
+                                        importObject.mapping.push({
+                                            mapFrom: '-1',
+                                            mapTo: column.id,
+                                            name: column.value
+                                        });
+                                    });
 
-              scope.$apply();
-            }
-          }
-        });
-      }
-    };
+                                    if (results.errors.length > 0) {
+                                        importObject.error = results.errors[0].message;
+                                    }
 
-    importExportService.generatePdf = function(board, messages, sortField) {
-      /* globals jsPDF */
-      var pdf = new jsPDF();
-      var currentHeight = 10;
+                                    scope.$apply();
+                                }
+                            }
+                        });
+                    }
+                };
 
-      $(board.columns).each(function(index, column) {
-        if (currentHeight > pdf.internal.pageSize.height - 10) {
-          pdf.addPage();
-          currentHeight = 10;
-        }
+                importExportService.generatePdf = function (board, messages, sortField) {
+                    /* globals jsPDF */
+                    var pdf = new jsPDF();
+                    var currentHeight = 10;
 
-        pdf.setFontType('bold');
-        currentHeight = currentHeight + 5;
-        pdf.text(column.value, 10, currentHeight);
-        currentHeight = currentHeight + 10;
-        pdf.setFontType('normal');
+                    $(board.columns).each(function (index, column) {
+                        if (currentHeight > pdf.internal.pageSize.height - 10) {
+                            pdf.addPage();
+                            currentHeight = 10;
+                        }
 
-        var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
+                        pdf.setFontType('bold');
+                        currentHeight = currentHeight + 5;
+                        pdf.text(column.value, 10, currentHeight);
+                        currentHeight = currentHeight + 10;
+                        pdf.setFontType('normal');
 
-        $(filteredArray).each(function(index2, message) {
-          if (message.type.id === column.id) {
-            var parsedText = pdf.splitTextToSize('- ' + message.text + ' (' + message.votes + ' votes)', 180);
-            var parsedHeight = pdf.getTextDimensions(parsedText).h;
-            pdf.text(parsedText, 10, currentHeight);
-            currentHeight = currentHeight + parsedHeight;
+                        var filteredArray = $filter('orderBy')(messages, importExportService.getSortFields(sortField));
 
-            if (currentHeight > pdf.internal.pageSize.height - 10) {
-              pdf.addPage();
-              currentHeight = 10;
-            }
-          }
-        });
-      });
+                        $(filteredArray).each(function (index2, message) {
+                            if (message.type.id === column.id) {
+                                var parsedText = pdf.splitTextToSize('- ' + message.text + ' (' + message.votes + ' votes)', 180);
+                                var parsedHeight = pdf.getTextDimensions(parsedText).h;
+                                pdf.text(parsedText, 10, currentHeight);
+                                currentHeight = currentHeight + parsedHeight;
 
-      pdf.save(board.boardId + '.pdf');
-    };
+                                if (currentHeight > pdf.internal.pageSize.height - 10) {
+                                    pdf.addPage();
+                                    currentHeight = 10;
+                                }
+                            }
+                        });
+                    });
 
-    var getColumnFieldObject = function(columnId) {
-      return {
-        type: {
-          id: columnId
-        }
-      };
-    };
+                    pdf.save(board.boardId + '.pdf');
+                };
 
-    var showCsvFileDownload = function(csvText, fileName) {
-      var blob = new Blob([csvText]);
-      var downloadLink = document.createElement('a');
-      downloadLink.href = window.URL.createObjectURL(blob, {type: 'text/csv'});
-      downloadLink.download = fileName;
-      
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
-    };
+                var getColumnFieldObject = function (columnId) {
+                    return {
+                        type: {
+                            id: columnId
+                        }
+                    };
+                };
 
-    importExportService.generateCsv = function(board, messages, sortField) {
+                var showCsvFileDownload = function (csvText, fileName) {
+                    var blob = new Blob([csvText]);
+                    var downloadLink = document.createElement('a');
+                    downloadLink.href = window.URL.createObjectURL(blob, {type: 'text/csv'});
+                    downloadLink.download = fileName;
 
-      var columns = board.columns.map(function(column) {
-        // Updated to use column.id, as columns could be any number when changed.
-        var columnMessages = $filter('filter')(messages, getColumnFieldObject(column.id));
-        var sortedColumnMessages = $filter('orderBy')(columnMessages, importExportService.getSortFields(sortField));
-        
-        var messagesText = sortedColumnMessages.map(function(message) { 
-          return message.text;
-        });
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);
+                };
 
-        var columnArray = [column.value].concat(messagesText);
+                importExportService.generateCsv = function (board, messages, sortField) {
 
-        return columnArray;
-      });
+                    var columns = board.columns.map(function (column) {
+                        // Updated to use column.id, as columns could be any number when changed.
+                        var columnMessages = $filter('filter')(messages, getColumnFieldObject(column.id));
+                        var sortedColumnMessages = $filter('orderBy')(columnMessages, importExportService.getSortFields(sortField));
 
-      var csvText = CsvService.buildCsvText(columns);
-      showCsvFileDownload(csvText, board.boardId + '.csv');
-    };
+                        var messagesText = sortedColumnMessages.map(function (message) {
+                            return message.text;
+                        });
 
-    return importExportService;
-  }]);
+                        var columnArray = [column.value].concat(messagesText);
+
+                        return columnArray;
+                    });
+
+                    var csvText = CsvService.buildCsvText(columns);
+                    showCsvFileDownload(csvText, board.boardId + '.csv');
+                };
+
+                return importExportService;
+            }]);
