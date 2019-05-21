ead4096d2be0bf2a270ccf484c205ee306564355
AJA <aleksandra.jachowicz@capgemini.com>
2019-05-21 19:38:16 +0200

    separate class for gif and meme

---
+++ js/directives/memeDialog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('fireideaz').directive('memeDialog', [function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'components/memeDialog.html'
+        };
+    }]
+);
